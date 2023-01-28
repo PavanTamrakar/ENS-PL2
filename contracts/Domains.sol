64dbebfd82defa57f2c27c4434f8176e7172f14e
PavanTamrakar <thepavantamrakar@gmail.com>
2023-01-28 12:45:42 +0530

    payableDone

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -2,6 +2,8 @@
 
 pragma solidity ^0.8.10;
 
+import {StringUtils} from "./libraries/StringUtils.sol";
+
 import "hardhat/console.sol";
 
 contract Domains {
@@ -9,13 +11,37 @@ contract Domains {
         console.log("My Polygon L2 ENS");
     }
 
+    string public tld;
+
     mapping(string => address) public domains;
 
     mapping(string => string) public records;
 
-    function register(string calldata name) public {
+    constructor(string memory topLD)payable{
+        tld = topLD;
+    }
+
+    function price(string calldata name) public pure returns(uint){
+        uint len = StringUtils.strlen(name);
+        require(len > 0, "Invalid");
+        if(len == 3){
+            return 5*10*17;
+        }else if (len == 4){
+            return 4*10*17;
+        }else{
+            return 1*10*17;
+        }
+    }
+
+    function register(string calldata name) public payable {
         require(domains[name] == address(0));
+        uint _price = price[name];
+
+        require(msg.value >= _price, "Not enough Matic");
+
         domains[name] = msg.sender;
+
+        console.log("%s has registered a domain!", msg.sender);
     }
 
     function getAddress(string calldata name) public view returns(address) {
@@ -30,5 +56,5 @@ contract Domains {
     function getRecord(string calldata name) public view returns(string memory) {
         return records[name];
     }
-    
+
 }
