04ab671240bd50fcf21bec5565610dd2279c3847
PavanTamrakar <thepavantamrakar@gmail.com>
2023-01-28 13:12:36 +0530

    test done

--- scripts/run.js
+++ scripts/run.js
@@ -1,20 +1,19 @@
 const main = async () => {
-    const [owner, randomPerson] = await hre.ethers.getSigners();
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
-    const domainContract = await domainContractFactory.deploy();
+    const domainContract = await domainContractFactory.deploy("drugs");
     await domainContract.deployed();
+
+    console.log("drugs Name Service Deployed")
     console.log("Contract deployed to:", domainContract.address);
-    console.log("Contract deployed by:", owner.address);
     
-    let txn = await domainContract.register("doom");
+    let txn = await domainContract.register("MonkOn", {value: hre.ethers.utils.parseEther('0.1')});
     await txn.wait();
+
+    const address = await domainContract.getAddress("MonkOn");
+    console.log("Owner of domain:", address);
   
-    const domainOwner = await domainContract.getAddress("doom");
-    console.log("Owner of domain:", domainOwner);
-
-    txn = await domainContract.connect(randomPerson).setRecord("doom", "Haha my domain now!");
-    await txn.wait();
-
+    const balance = await hre.ethers.provider.getBalance(domainContract.address);
+    console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
   }
   
   const runMain = async () => {
