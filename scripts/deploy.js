ad50612aa17b9e06110313a4341793e91f153ea4
PavanTamrakar <thepavantamrakar@gmail.com>
2023-01-29 14:14:59 +0530

    deploy

---
+++ scripts/deploy.js
@@ -0,0 +1,34 @@
+const main = async () => {
+    const domainContractFactory = await hre.ethers.getContractFactory('Domains');
+    const domainContract = await domainContractFactory.deploy("drugs");
+    await domainContract.deployed();
+
+    console.log("drugs Name Service Deployed")
+    console.log("Contract deployed to:", domainContract.address);
+    
+    let txn = await domainContract.register("MonkOn", {value: hre.ethers.utils.parseEther('0.1')});
+    await txn.wait();
+    console.log("Minted MonkOn.drugs");
+
+    txn = await domainContract.setRecord("MonkOn", "Monks are always On something.");
+    await txn.wait();
+    console.log("Set record for MonkOn.drugs");
+
+    const address = await domainContract.getAddress("MonkOn");
+    console.log("Owner of domain:", address);
+  
+    const balance = await hre.ethers.provider.getBalance(domainContract.address);
+    console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
+  }
+  
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  };
+  
+  runMain();
