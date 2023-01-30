751f39f76ba1b457446856349426a3aba60af5c8
PavanTamrakar <thepavantamrakar@gmail.com>
2023-01-30 17:30:48 +0530

    deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
   network : {
     mumbai : {
       url : "https://stylish-side-knowledge.matic-testnet.discover.quiknode.pro/86ce51f25bcc9f03ce5d4738f446fc3b623f3b1f/",
-      accounts : ["0e8f28e64762e22c60a61087a2fa7bde110782573c7f290886dc7f14ade07047"]
+      accounts : ["0e8f28e64762e22c60a61087a2fa7bde110782573c7f290886dc7f14ade07047"],
     }
   }
 };
