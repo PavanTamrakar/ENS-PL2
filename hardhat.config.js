7cc7c58e6c4a827dd4fb9662c41f5e54ea9736e7
PavanTamrakar <thepavantamrakar@gmail.com>
2023-01-30 22:49:22 +0530

    deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,9 +4,12 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
   solidity: "0.8.10",
   network : {
+    hardhat : {
+    },
+    loggingEnabled : true,
     mumbai : {
       url : "https://stylish-side-knowledge.matic-testnet.discover.quiknode.pro/86ce51f25bcc9f03ce5d4738f446fc3b623f3b1f/",
       accounts : ["0e8f28e64762e22c60a61087a2fa7bde110782573c7f290886dc7f14ade07047"],
-    }
-  }
+    },
+  },
 };
